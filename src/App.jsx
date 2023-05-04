face34d4799d36f87ff3081a2d519d987bde0e09
Ferenc Feher <ferencfeher38@gmail.com>
2023-05-04 20:53:27 +0200

    Store the notes in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-import React, { useState } from "react";
-import Header from "./Header";
-import CreateArea from "./CreateArea";
-import Note from "./Note";
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import CreateArea from './CreateArea';
+import Note from './Note';
 
 const App = function () {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(getInitialNotes());
+
+  function getInitialNotes() {
+    const tempNotes = localStorage.getItem('notes');
+    const savedNotes = JSON.parse(tempNotes);
+    return savedNotes || [];
+  }
+
+  useEffect(() => {
+    const tempNotes = JSON.stringify(notes);
+    localStorage.setItem('notes', tempNotes);
+  }, [notes]);
 
   const addNote = function (newNote) {
-    setNotes((prevNotes) => {
+    setNotes(prevNotes => {
       return [...prevNotes, newNote];
     });
   };
 
   const deleteNote = function (id) {
-    setNotes((prevNotes) => {
+    setNotes(prevNotes => {
       return prevNotes.filter((_, index) => {
         return index !== id;
       });
